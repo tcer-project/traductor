21fb9d302f50f5bbe1538889a13304876d9f16ba
{ROA-DEV} <105471756+tcer12@users.noreply.github.com>
2024-03-22 17:08:26 -0400

    Add files via upload

---
+++ asset/js/script.js
@@ -0,0 +1,277 @@
+const dropdowns = document.querySelectorAll(".dropdown-container"),
+  inputLanguageDropdown = document.querySelector("#input-language"),
+  outputLanguageDropdown = document.querySelector("#output-language");
+
+function populateDropdown(dropdown, options) {
+  dropdown.querySelector("ul").innerHTML = "";
+  options.forEach((option) => {
+    const li = document.createElement("li");
+    const title = option.name + " (" + option.native + ")";
+    li.innerHTML = title;
+    li.dataset.value = option.code;
+    li.classList.add("option");
+    dropdown.querySelector("ul").appendChild(li);
+  });
+}
+
+populateDropdown(inputLanguageDropdown, languages);
+populateDropdown(outputLanguageDropdown, languages);
+
+dropdowns.forEach((dropdown) => {
+  dropdown.addEventListener("click", (e) => {
+    dropdown.classList.toggle("active");
+  });
+
+  dropdown.querySelectorAll(".option").forEach((item) => {
+    item.addEventListener("click", (e) => {
+      //remove active class from current dropdowns
+      dropdown.querySelectorAll(".option").forEach((item) => {
+        item.classList.remove("active");
+      });
+      item.classList.add("active");
+      const selected = dropdown.querySelector(".selected");
+      selected.innerHTML = item.innerHTML;
+      selected.dataset.value = item.dataset.value;
+      translate();
+    });
+  });
+});
+
+document.addEventListener("click", (e) => {
+  dropdowns.forEach((dropdown) => {
+    if (!dropdown.contains(e.target)) {
+      dropdown.classList.remove("active");
+    }
+  });
+});
+
+const swapBtn = document.querySelector(".swap-position"),
+  inputLanguage = inputLanguageDropdown.querySelector(".selected"),
+  outputLanguage = outputLanguageDropdown.querySelector(".selected"),
+  inputTextElem = document.querySelector("#input-text"),
+  outputTextElem = document.querySelector("#output-text");
+
+swapBtn.addEventListener("click", (e) => {
+  const temp = inputLanguage.innerHTML;
+  inputLanguage.innerHTML = outputLanguage.innerHTML;
+  outputLanguage.innerHTML = temp;
+
+  const tempValue = inputLanguage.dataset.value;
+  inputLanguage.dataset.value = outputLanguage.dataset.value;
+  outputLanguage.dataset.value = tempValue;
+
+  //swap text
+  const tempInputText = inputTextElem.value;
+  inputTextElem.value = outputTextElem.value;
+  outputTextElem.value = tempInputText;
+
+  translate();
+});
+
+function translate() {
+  const inputText = inputTextElem.value;
+  const inputLanguage =
+    inputLanguageDropdown.querySelector(".selected").dataset.value;
+  const outputLanguage =
+    outputLanguageDropdown.querySelector(".selected").dataset.value;
+  const url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=${inputLanguage}&tl=${outputLanguage}&dt=t&q=${encodeURI(
+    inputText
+  )}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+      outputTextElem.value = json[0].map((item) => item[0]).join("");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+inputTextElem.addEventListener("input", (e) => {
+  //limit input to 3000 characters
+  if (inputTextElem.value.length > 5000) {
+    inputTextElem.value = inputTextElem.value.slice(0, 5000);
+  }
+  translate();
+});
+
+const uploadDocument = document.querySelector("#upload-document"),
+  uploadTitle = document.querySelector("#upload-title");
+
+uploadDocument.addEventListener("change", (e) => {
+  const file = e.target.files[0];
+  if (
+    file.type === "application/pdf" ||
+    file.type === "text/plain" ||
+    file.type === "application/msword" ||
+    file.type ===
+      "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+  ) {
+    uploadTitle.innerHTML = file.name;
+    const reader = new FileReader();
+    reader.readAsText(file);
+    reader.onload = (e) => {
+      inputTextElem.value = e.target.result;
+      translate();
+    };
+  } else {
+    alert("Please upload a valid file");
+  }
+});
+
+const downloadBtn = document.querySelector("#download-btn");
+
+downloadBtn.addEventListener("click", (e) => {
+  const outputText = outputTextElem.value;
+  const outputLanguage =
+    outputLanguageDropdown.querySelector(".selected").dataset.value;
+  if (outputText) {
+    const blob = new Blob([outputText], { type: "text/plain" });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.download = `translated-to-${outputLanguage}.txt`;
+    a.href = url;
+    a.click();
+  }
+});
+
+//Character limit
+document.getElementById('input-text').addEventListener('input', function() {
+  let maxLength = 5000; // Número máximo de caracteres permitidos
+  let currentLength = this.value.length;
+  let counter = document.getElementById('input-chars');
+  
+  // Actualizar contador de caracteres restantes
+  counter.textContent = maxLength - currentLength;
+  
+  // Limitar la entrada si excede el máximo
+  if (currentLength >= maxLength) {
+      this.value = this.value.slice(0, maxLength);
+      counter.textContent = 0;
+      alert('El texto supera el límite de caracteres permitido');
+  }
+});
+
+(function(){
+  const listElements = document.querySelectorAll('.menu__item--show');
+  const list = document.querySelector('.menu__links');
+  const menu = document.querySelector('.menu__hamburguer');
+
+  const addClick = ()=>{
+      listElements.forEach(element =>{
+          element.addEventListener('click', ()=>{
+
+              
+              let subMenu = element.children[1];
+              let height = 0;
+              element.classList.toggle('menu__item--active');
+
+
+              if(subMenu.clientHeight === 0){
+                  height = subMenu.scrollHeight;
+              }
+
+              subMenu.style.height = `${height}px`;
+
+          });
+      });
+  }
+
+  const deleteStyleHeight = ()=>{
+      listElements.forEach(element=>{
+
+          if(element.children[1].getAttribute('style')){
+              element.children[1].removeAttribute('style');
+              element.classList.remove('menu__item--active');
+          }
+
+      });
+  }
+
+
+  window.addEventListener('resize', ()=>{
+      if(window.innerWidth > 800){
+          deleteStyleHeight();
+          if(list.classList.contains('menu__links--show'))
+              list.classList.remove('menu__links--show');
+
+      }else{
+          addClick();
+      }
+  });
+
+  if(window.innerWidth <= 800){
+      addClick();
+  }
+
+  menu.addEventListener('click', ()=> list.classList.toggle('menu__links--show'));
+
+
+
+})();
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const slider = document.querySelector('.slider');
+  const slides = document.querySelectorAll('.slide');
+  const slideWidth = slides[0].offsetWidth;
+
+  let currentIndex = 0;
+  let interval;
+
+  function nextSlide() {
+      currentIndex = (currentIndex + 1) % slides.length;
+      const offset = -currentIndex * slideWidth;
+      slider.style.transform = `translateX(${offset}px)`;
+  }
+
+  function startSlider() {
+      interval = setInterval(nextSlide, 5000); // Cambiar de slide cada 5 segundos
+  }
+
+  function stopSlider() {
+      clearInterval(interval);
+  }
+
+  startSlider(); // Iniciar slider al cargar la página
+
+  // Pausar slider al pasar el mouse sobre el slider
+  slider.addEventListener('mouseenter', stopSlider);
+  slider.addEventListener('mouseleave', startSlider);
+});
+
+/*Animaciones*/
+
+anime({
+  targets: '.texto',
+  translateY: '50px',
+  easing: 'easeInOutQuad',
+  duration: 1000,
+  loop: false
+});
+
+anime({
+  targets: '.center',
+  translateY: '50px',
+  easing: 'easeInOutQuad',
+  duration: 1000,
+  loop: false
+});
+
+anime({
+  targets: '.cardo1',
+  translateX: '50px',
+  easing: 'easeInOutQuad',
+  duration: 1000,
+  loop: false
+});
+
+anime({
+  targets: '.cardo2',
+  translateX: '-50px',
+  easing: 'easeInOutQuad',
+  duration: 1000,
+  loop: false
+});
+
+
---
+++ asset/js/secundary.js
@@ -0,0 +1,81 @@
+
+(function(){
+    const listElements = document.querySelectorAll('.menu__item--show');
+    const list = document.querySelector('.menu__links');
+    const menu = document.querySelector('.menu__hamburguer');
+  
+    const addClick = ()=>{
+        listElements.forEach(element =>{
+            element.addEventListener('click', ()=>{
+  
+                
+                let subMenu = element.children[1];
+                let height = 0;
+                element.classList.toggle('menu__item--active');
+  
+  
+                if(subMenu.clientHeight === 0){
+                    height = subMenu.scrollHeight;
+                }
+  
+                subMenu.style.height = `${height}px`;
+  
+            });
+        });
+    }
+  
+    const deleteStyleHeight = ()=>{
+        listElements.forEach(element=>{
+  
+            if(element.children[1].getAttribute('style')){
+                element.children[1].removeAttribute('style');
+                element.classList.remove('menu__item--active');
+            }
+  
+        });
+    }
+  
+  
+    window.addEventListener('resize', ()=>{
+        if(window.innerWidth > 800){
+            deleteStyleHeight();
+            if(list.classList.contains('menu__links--show'))
+                list.classList.remove('menu__links--show');
+  
+        }else{
+            addClick();
+        }
+    });
+  
+    if(window.innerWidth <= 800){
+        addClick();
+    }
+  
+    menu.addEventListener('click', ()=> list.classList.toggle('menu__links--show'));
+  
+  
+  
+  })();
+  
+  $('.testimonials-container').owlCarousel({
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:6000,
+    margin:10,
+    nav:true,
+    navText:["<i class='fa-solid fa-arrow-left'></i>",
+             "<i class='fa-solid fa-arrow-right'></i>"],
+    responsive:{
+        0:{
+            items:1,
+            nav:false
+        },
+        600:{
+            items:1,
+            nav:true
+        },
+        768:{
+            items:2
+        },
+    }
+  })
